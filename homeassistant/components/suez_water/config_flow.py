9e046a94706440eb67579e18bde135e235558555
jb101010-2 <dev.julien.basson@gmail.com>
2024-12-23 22:04:26 +0100

    Suez_water: use meter id as unique_id

--- homeassistant/components/suez_water/config_flow.py
+++ homeassistant/components/suez_water/config_flow.py
@@ -55,15 +55,17 @@ async def validate_input(data: dict[str, Any]) -> None:
 class SuezWaterConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Suez Water."""
 
-    VERSION = 1
+    VERSION = 2
+    MINOR_VERSION = 0
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
-        """Handle the initial step."""
+        """Handle initial setup step."""
         errors: dict[str, str] = {}
+
         if user_input is not None:
-            await self.async_set_unique_id(user_input[CONF_USERNAME])
+            await self.async_set_unique_id(user_input[CONF_COUNTER_ID])
             self._abort_if_unique_id_configured()
             try:
                 await validate_input(user_input)
@@ -78,7 +80,7 @@ class SuezWaterConfigFlow(ConfigFlow, domain=DOMAIN):
                 errors["base"] = "unknown"
             else:
                 return self.async_create_entry(
-                    title=user_input[CONF_USERNAME], data=user_input
+                    title=user_input[CONF_COUNTER_ID], data=user_input
                 )
 
         return self.async_show_form(
@@ -98,4 +100,4 @@ class InvalidAuth(HomeAssistantError):
 
 
 class CounterNotFound(HomeAssistantError):
-    """Error to indicate we cannot automatically found the counter id."""
+    """Error to indicate we failed to automatically find the counter id."""
